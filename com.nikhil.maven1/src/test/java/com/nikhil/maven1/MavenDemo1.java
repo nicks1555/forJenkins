833380084bb8a710c989a945a51203d945310fed
nicks1555 <nicks1555@gmail.com>
2022-12-12 14:28:26 +0530

    nikhil : commiting and pushing on github

---
+++ com.nikhil.maven1/src/test/java/com/nikhil/maven1/MavenDemo1.java
@@ -0,0 +1,12 @@
+package com.nikhil.maven1;
+
+import org.testng.annotations.Test;
+
+public class MavenDemo1 {
+	
+	@Test
+	public void m1() {
+		System.out.println("Welcome to Maven world......");
+	}
+
+}
